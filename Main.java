dff8747de089fec2dbd81511dc0ff902eb59ad83
Jack Tschetter <95504211+JackTschetter@users.noreply.github.com>
2023-07-26 15:54:35 -0500

    Delete Main.java

--- Main.java
+++
@@ -1,6 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        OthelloState t = new OthelloState(new Player(-1), new Player(1), 0);
-        t.play_game(null, null);
-    }
-}
